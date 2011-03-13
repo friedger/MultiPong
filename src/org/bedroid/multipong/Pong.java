0b3a2b623df2064b168e3a8fa3a6643922adcbcb
moandji.ezana <mwanji@gmail.com>
2011-03-13 10:48:09 +0100

    project created

---
+++ src/org/bedroid/multipong/Pong.java
@@ -0,0 +1,13 @@
+package org.bedroid.multipong;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Pong extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
