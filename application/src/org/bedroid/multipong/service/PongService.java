7d01978921bb5f35134fccce712f92c6b31f9cac
pjv <ezelspinguin@gmail.com>
2011-03-13 14:02:37 +0100

    ...and some extra files for the network.

---
+++ application/src/org/bedroid/multipong/service/PongService.java
@@ -0,0 +1,21 @@
+package org.bedroid.multipong.service;
+
+import org.alljoyn.bus.BusException;
+import org.alljoyn.bus.BusObject;
+
+import android.util.Log;
+
+/**
+ * @author pjv
+ * 
+ *         Local implementation of the PongServiceInterface.
+ * 
+ */
+public class PongService extends Object implements PongServiceInterface,
+		BusObject {
+	private static final String TAG = "PongService";
+
+	public void Hello() throws BusException {
+		Log.i(TAG, "HELLOOOOOOOOOO");
+	}
+}
