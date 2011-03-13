e7d37a9a40f4ffacf0fb173a4a55924f3099447c
pjv <ezelspinguin@gmail.com>
2011-03-13 18:23:54 +0100

    Added some game logic and link move received signal

--- application/src/org/bedroid/multipong/BallMove.java
+++ application/src/org/bedroid/multipong/BallMove.java
@@ -1,12 +1,23 @@
 package org.bedroid.multipong;
 
+import org.alljoyn.bus.annotation.Position;
+
 import android.graphics.Point;
 
 public class BallMove {
 
+	@Position(0)
 	public Point origin;
+	@Position(1)
 	public int originPaddleId;
+	@Position(2)
 	private int outgoingAngle;
+
+	public BallMove() {// TODO: remove empty constructor?
+		this.origin = new Point();
+		this.outgoingAngle = 0;
+		this.originPaddleId = 0;
+	}
 
 	public BallMove(Point origin, int outgoingAngle, int originPaddleId) {
 		this.origin = origin;
--- application/src/org/bedroid/multipong/service/PongSignal.java
+++ application/src/org/bedroid/multipong/service/PongSignal.java
@@ -3,8 +3,11 @@ package org.bedroid.multipong.service;
 import org.alljoyn.bus.BusException;
 import org.alljoyn.bus.BusObject;
 import org.alljoyn.bus.annotation.BusSignalHandler;
+import org.bedroid.multipong.BallMove;
+import org.bedroid.multipong.PongActivity;
 
-import android.util.Log;
+import android.os.Handler;
+import android.os.Message;
 
 /**
  * Signal interface (for emitter) and signal handler in one class
@@ -16,15 +19,25 @@ public class PongSignal implements PongSignalInterface, BusObject {
 
 	private static final String TAG = "PongSignal";
 
+	private Handler handler;
+
+	/**
+	 * 
+	 */
+	public PongSignal(Handler handler) {
+		super();
+		this.handler = handler;
+	}
+
 	@Override
-	public void Move(int direction) throws BusException {
+	public void Move(BallMove ballMove) throws BusException {
 		// no code here
 	}
 
 	@BusSignalHandler(iface = "org.bedroid.multipong.signal", signal = "Move")
-	public void MoveReceived(int direction) {
-		Log.i(TAG, "Signal received " + direction);
-
+	public void MoveReceived(BallMove ballMove) {
+		Message msg = handler.obtainMessage(PongActivity.MESSAGE_MOVE_RECEIVED);
+		handler.sendMessage(msg);
 	}
 
 }
